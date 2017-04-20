96b9552c10aca09a1b8ffd7fe74e5927f8e562c4
wu <adwerrd@hotmail.com>
2017-04-20 18:01:03 +0800

    mobify remove domain

--- src/components/service/HOST.js
+++ src/components/service/HOST.js
@@ -1,5 +1,4 @@
 export const HOST = {
     awsHost: 's2.i.qingcdn.com',
-    apiHost: 'frontendapi-stats.bscstorage.com:9090',
-    domain: 'www.bscstorage.com:8080'
+    apiHost: 'frontendapi-stats.bscstorage.com:9090'
 }
