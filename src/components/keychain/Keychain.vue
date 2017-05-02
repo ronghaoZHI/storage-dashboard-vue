a6e4b4bcc71d5bce012cbb5d1f8c06d6508ef87d
wu <adwerrd@hotmail.com>
2017-05-02 20:18:55 +0800

    bugfix keychain table width

--- src/components/keychain/Keychain.vue
+++ src/components/keychain/Keychain.vue
@@ -20,6 +20,7 @@ export default {
                 },
                 {
                     title: 'Create time',
+                    width: 140,
                     key: 'ts'
                 }
             ],
