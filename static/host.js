a5ee32eb32116dec576d4ee62fbad80ed352cbdc
wu <adwerrd@hotmail.com>
2017-05-04 19:49:00 +0800

    Build release

--- static/host.js
+++ static/host.js
@@ -1,6 +1,6 @@
 // for ansible build
 // this way is very ugly
 window.dashboard_conf = {
-    "apiHost": 'frontendapi-stats.bscstorage.com:9090',
-    "awsHost": 's2.i.qingcdn.com'
+    awsHost: '101.200.90.34',
+    apiHost: '101.200.190.192:9090'
 };
