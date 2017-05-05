1d44fb10642823b2295fa93d208a923faa7c16a6
wu <adwerrd@hotmail.com>
2017-05-05 14:31:28 +0800

    mofify font-family for windows

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,6 @@ body {
 }
 
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
