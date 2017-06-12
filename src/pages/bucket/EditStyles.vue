d4d64616dfdaae359db1aee94afab33d0fc40520
zyl <949472497@qq.com>
2017-06-12 10:41:06 +0800

    ZYL modify overlay font family

--- src/pages/bucket/EditStyles.vue
+++ src/pages/bucket/EditStyles.vue
@@ -83,7 +83,7 @@
                                 </div>
                                 <span class="form-label">{{$t("STORAGE.TEXT_STYLE")}} : </span>
                                 <Select v-model="watermarker.fontStyle.font_family" style="width:135px;margin-right:10px;">
-                                    <Option v-for="item in fontList" :value="item" :key="item">{{ item }}</Option>
+                                    <Option v-for="item in fontList" :value="item.value" :key="item">{{ item.label }}</Option>
                                 </Select>
                                 <div class="input-text-box">
                                     <input type='number' v-model="watermarker.fontStyle.font_size">
@@ -231,7 +231,7 @@ export default {
         return {
             transformation: '',
             formatList: ['original', 'png', 'webp', 'jpeg', 'jpg'],
-            fontList: ['宋体', '黑体', '楷体', '隶书', '幼园', '仿宋', 'Arial', 'Georgia', 'Helvetica', 'Times-new-roman'],
+            fontList: allFontList,
             fontColorPicker: false,
             watermarker: watermarkerDefult,
             general: generalDefult,
@@ -377,9 +377,273 @@ export default {
         // the contents array need refresh when the $route value changed
         '$route' (to, from) {
             to.path !== from.path && this.getData()
+        },
+        'watermarker.open' (to, from) {
+            console.log('watermarker', to)
         }
     }
 }
+const allFontList = [{
+    value: 'Songti SC',
+    label: '宋体'
+},
+{
+    value: 'Heiti SC',
+    label: '黑体'
+},
+{
+    value: 'Kaiti SC',
+    label: '楷体'
+},
+{
+    value: 'Microsoft YaHei',
+    label: '微软雅黑'
+},
+{
+    value: 'Adobe Song Std',
+    label: 'Adobe Song Std'
+},
+{
+    value: 'Adobe Heiti Std',
+    label: 'Adobe Heiti Std'
+},
+{
+    value: 'Adobe Kaiti Std',
+    label: 'Adobe Kaiti Std'
+},
+{
+    value: 'Adobe Fangsong Std',
+    label: 'Adobe Fangsong Std'
+},
+{
+    value: 'Batang',
+    label: 'Batang'
+},
+{
+    value: 'Bookshelf Symbol 7',
+    label: 'Bookshelf Symbol 7'
+},
+{
+    value: 'Brush Script MT',
+    label: 'Brush Script MT'
+},
+{
+    value: 'Calibri',
+    label: 'Calibri'
+},
+{
+    value: 'Cambria',
+    label: 'Cambria'
+},
+{
+    value: 'Candara',
+    label: 'Candara'
+},
+{
+    value: 'Candelita',
+    label: 'Candelita'
+},
+{
+    value: 'Consolas',
+    label: 'Consolas'
+},
+{
+    value: 'Constantia',
+    label: 'Constantia'
+},
+{
+    value: 'Corbel',
+    label: 'Corbel'
+},
+{
+    value: 'Franklin Gothic Book',
+    label: 'Franklin Gothic Book'
+},
+{
+    value: 'Franklin Gothic Medium',
+    label: 'Franklin Gothic Medium'
+},
+{
+    value: 'Gill Sans MT',
+    label: 'Gill Sans MT'
+},
+{
+    value: 'Gulim',
+    label: 'Gulim'
+},
+{
+    value: 'Hannotate SC',
+    label: 'Hannotate SC'
+},
+{
+    value: 'Hannotate TC',
+    label: 'Hannotate TC'
+},
+{
+    value: 'HanziPen SC',
+    label: 'HanziPen SC'
+},
+{
+    value: 'HanziPen TC',
+    label: 'HanziPen TC'
+},
+{
+    value: 'Hiragino Sans GB',
+    label: 'Hiragino Sans GB'
+},
+{
+    value: 'Lantinghei SC',
+    label: 'Lantinghei SC'
+},
+{
+    value: 'Lantinghei TC',
+    label: 'Lantinghei TC'
+},
+{
+    value: 'Libian SC',
+    label: 'Libian SC'
+},
+{
+    value: 'Lucida Console',
+    label: 'Lucida Console'
+},
+{
+    value: 'Lucida Sans Unicode',
+    label: 'Lucida Sans Unicode'
+},
+{
+    value: 'MJNgai PRC',
+    label: 'MJNgai PRC'
+},
+{
+    value: 'MS Gothic',
+    label: 'MS Gothic'
+},
+{
+    value: 'MS Mincho',
+    label: 'MS Mincho'
+},
+{
+    value: 'MS PGothic',
+    label: 'MS PGothic'
+},
+{
+    value: 'MS PMincho',
+    label: 'MS PMincho'
+},
+{
+    value: 'MS Reference Sans Serif',
+    label: 'MS Reference Sans Serif'
+},
+{
+    value: 'Marlett',
+    label: 'Marlett'
+},
+{
+    value: 'Meiryo',
+    label: 'Meiryo'
+},
+{
+    value: 'Microsoft YaHei',
+    label: 'Microsoft YaHei'
+},
+{
+    value: 'PMingLiU',
+    label: 'PMingLiU'
+},
+{
+    value: 'Perpetua',
+    label: 'Perpetua'
+},
+{
+    value: 'STFangsong',
+    label: 'STFangsong'
+},
+{
+    value: 'STHeiti',
+    label: 'STHeiti'
+},
+{
+    value: 'STHupo',
+    label: 'STHupo'
+},
+{
+    value: 'STKaiti',
+    label: 'STKaiti'
+},
+{
+    value: 'STLiti',
+    label: 'STLiti'
+},
+{
+    value: 'STSong',
+    label: 'STSong'
+},
+{
+    value: 'STXingkai',
+    label: 'STXingkai'
+},
+{
+    value: 'STXinwei',
+    label: 'STXinwei'
+},
+{
+    value: 'STZhongsong',
+    label: 'STZhongsong'
+},
+{
+    value: 'SentyPaperCut',
+    label: 'SentyPaperCut'
+},
+{
+    value: 'SimHei',
+    label: 'SimHei'
+},
+{
+    value: 'SimSun',
+    label: 'SimSun'
+},
+{
+    value: 'SimSun-ExtB',
+    label: 'SimSun-ExtB'
+},
+{
+    value: 'Songti TC',
+    label: 'Songti TC'
+},
+{
+    value: 'Source Han Sans CN',
+    label: 'Source Han Sans CN'
+},
+{
+    value: 'Tw Cen MT',
+    label: 'Tw Cen MT'
+},
+{
+    value: 'Villasukat',
+    label: 'Villasukat'
+},
+{
+    value: 'Wawati SC',
+    label: 'Wawati SC'
+},
+{
+    value: 'Weibei SC',
+    label: 'Weibei SC'
+},
+{
+    value: 'Xingkai SC',
+    label: 'Xingkai SC'
+},
+{
+    value: 'Yuanti SC',
+    label: 'Yuanti SC'
+},
+{
+    value: 'Yuppy SC',
+    label: 'Yuppy SC'
+}
+]
 const I2J = {
     c_: 'crop',
     w_: 'width',
@@ -400,7 +664,7 @@ const I2J = {
     t_: 'transformation'
 }
 const defaultFontColor = {
-    hex: '#000000'
+    hex: '#BF4040'
 }
 const generalDefult = {
     crop: 'noCrop',
@@ -416,7 +680,7 @@ const watermarkerDefult = {
     text: '',
     fontStyle: {
         text: '',
-        font_family: '宋体',
+        font_family: 'Songti SC',
         font_size: 16
     },
     gravity: 'north_west',
