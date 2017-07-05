8bc46c1486173367a757d8eeffb5eb5083595028
lan <adwerrd@hotmail.com>
2017-07-05 17:35:20 +0800

    rw keychain card view

--- src/pages/keychain/KeychainCard.vue
+++ src/pages/keychain/KeychainCard.vue
@@ -1,12 +1,22 @@
 <template>
-    <Tooltip content="Click to copy keys">
-        <div class="bsc-keychain-card" :data-clipboard-text="`accesskey:${keychain.accesskey},secretkey:${keychain.secretkey}`" v-clip>
-            <span class="info"><Icon type="key"></Icon>{{keychain.accesskey}}</span>
-            <span class="info"><Icon type="locked"></Icon>{{keychain.secretkey}}</span>
-            <span class="ts">{{keychain.ts}}</span>
-            <span class="status" :class="{ alive: keychain.is_del === 0 }"><Icon type="record"></Icon></span>
+    <div class="bsc-keychain-card">
+        <div class="header">
+            <Icon :class="{ alive: keychain.is_del === 0 }" type="record"></Icon>
+            <span>{{keychain.ts}}</span>
         </div>
-    </Tooltip>
+        <Tooltip content="Click to copy accesskey">
+            <div class="content" @click="copy" :data-clipboard-text="keychain.accesskey" v-clip>
+                <Icon type="key"></Icon>
+                <span class="keys">{{keychain.accesskey}}</span>
+            </div>
+         </Tooltip>
+         <Tooltip content="Click to copy secretkey">
+            <div class="content" @click="copy" :data-clipboard-text="keychain.secretkey" v-clip>
+                <Icon type="locked"></Icon>
+                <span class="keys">{{keychain.secretkey}}</span>
+            </div>
+         </Tooltip>
+    </div>
 </template>
 <script>
 import Clipboard from 'clipboard'
@@ -21,73 +31,66 @@ export default {
             }
         }
     },
-    props: ['keychain']
+    props: ['keychain'],
+    methods: {
+        copy () {
+            this.$Message.success('Copied!')
+        }
+    }
 }
 
 </script>
 <style lang="less" scoped>
 
 @import '../../styles/index.less';
-@keychain-card-width: 300px;
-@keychain-card-height: 100px;
-@keychain-card-text-color: #657180;
-@keychain-card-border-color: #d7dde4;
-@keychain-card-input-backgrand: #f5f7f9;
-@keychain-card-input-backgrand-hover: #d9d9d9;
+@keychain-card-width: 370px;
+@keychain-card-height: 110px;
+@keychain-card-header-height: 30px;
+@keychain-card-text-color: #8391a4;
+@keychain-card-backgrand: #f8fbfb;
+@keychain-card-header-backgrand: #eff3f7;
+@keychain-card-backgrand-hover: #d9d9d9;
 
 .@{css-prefix}keychain-card {
     .wh(@keychain-card-width, @keychain-card-height);
-    position: relative;
-    background-color: @keychain-card-input-backgrand;
-    border-radius: @common-radius;
-    border: 1px solid @keychain-card-border-color;
+    background-color: @keychain-card-backgrand;
     .sc(14px, @keychain-card-text-color);
-    padding-top: 20px;
+    box-shadow:2px 2px 2px #e2e2e3;
     margin: 6px 3px;
     cursor: pointer;
 
-    &:hover {
-        background-color: @keychain-card-input-backgrand-hover;
+    .header {
+        .wh(100%,@keychain-card-header-height);
+        .fb(space-between,center);
+        padding: 0 12px;
+        background-color: @keychain-card-header-backgrand;
     }
 
-    .info {
-        display: inline-block;
-        .wh(90%, @keychain-card-height / 2 - 15);
-        line-height: @keychain-card-height / 2 - 15;
-        vertical-align: middle;
-        padding: 6px 16px;
-        text-align: left;
-        text-overflow: ellipsis;
-        overflow:hidden;
-        white-space:nowrap;
-
+    .content {
+        .wh(100%,(@keychain-card-height - @keychain-card-header-height - 10) / 2);
+        .fb(flex-start,center);
+        padding: 10px 12px 0 12px;
+        
         i {
             text-align: center;
             position: relative;
-            top: 1px;
             padding-right: 10px;
             width: 20px;
         }
-    }
 
-    .ts {
-        position: absolute;
-        color: @keychain-card-text-color;
-        top: 6px;
-        right: 12px;
-        font-size: 12px;
-    }
+        .keys {
+            text-overflow: ellipsis;
+            overflow:hidden;
+            white-space:nowrap;
+        }
 
-    .status {
-        position: absolute;
-        color: @keychain-card-text-color;
-        top: 6px;
-        left: 12px;
-        font-size: 12px;
+        &:hover {
+            color: #50575c;
+        }
     }
 
     .alive {
-        color: #00924c;
+        color: #22d067;
     }
 }
 
