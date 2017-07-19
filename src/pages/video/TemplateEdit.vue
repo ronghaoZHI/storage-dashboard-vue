6ee2f19eb1cbf07c5d3301a60b78a35b97787519
zyl <949472497@qq.com>
2017-07-19 15:12:12 +0800

    add template relationship between container & codec

--- src/pages/video/TemplateEdit.vue
+++ src/pages/video/TemplateEdit.vue
@@ -41,7 +41,7 @@
             <div class="form-item">
                 <span class="form-label">编码方式 : </span>
                 <Radio-group v-model="template.Video.Codec">
-                    <Radio v-for='codec in videoCodecList' :key="codec.value" :label='codec.value'>{{codec.name}}</Radio>
+                    <Radio v-for='codec in videoCodecList' v-if="codec.container.includes(template.Container)" :key="codec.value" :label='codec.value'>{{codec.name}}</Radio>
                 </Radio-group>
             </div>
             <div class="form-item" v-if="template.Video.Codec === 'H.264'">
@@ -103,7 +103,7 @@
             <div class="form-item">
                 <span class="form-label">编码方式 : </span>
                 <Radio-group v-model="template.Audio.Codec">
-                    <Radio v-for='codec in audioCodecList' :key="codec.value" :label='codec.value'>{{codec.name}}</Radio>
+                    <Radio v-for='codec in audioCodecList' v-if="codec.container.includes(template.Container)" :key="codec.value" :label='codec.value'>{{codec.name}}</Radio>
                 </Radio-group>
             </div>
             <div class="form-item" v-if="template.Audio.Codec === 'AAC'">
@@ -149,12 +149,12 @@ export default {
             template: _.cloneDeep(templateDefult),
             auxiliary: _.cloneDeep(auxiliaryDefult),
             containerList: ['flac', 'flv', 'gif', 'mp3', 'mp4', 'mpg', 'ts'],
-            videoCodecList: [{name: '不变', value: 'auto'}, {name: 'gif', value: 'gif'}, {name: 'H.264', value: 'H.264'}, {name: 'H.265', value: 'H.265'}, {name: 'mpeg2', value: 'mpeg2'}],
+            videoCodecList: [{name: '不变', value: 'auto', container: ['flac', 'flv', 'gif', 'mp3', 'mp4', 'mpg', 'ts']}, {name: 'gif', value: 'gif', container: ['gif']}, {name: 'H.264', value: 'H.264', container: ['flv', 'mp4', 'ts']}, {name: 'H.265', value: 'H.265', container: ['flv', 'mp4', 'ts']}, {name: 'mpeg2', value: 'mpeg2', container: ['mpg']}],
             videoProfileList: ['baseline', 'main', 'high'],
             videoLevelList: ['1', '1b', '1.1', '1.2', '1.3', '2', '2.1', '2.2', '3', '3.1', '3.2', '4', '4.1'],
             videoFrameRateList: [{name: '不变', value: 'auto'}, {name: '10', value: '10'}, {name: '15', value: '15'}, {name: '23.97', value: '23.97'}, {name: '24', value: '24'}, {name: '25', value: '25'}, {name: '29.97', value: '29.97'}, {name: '30', value: '30'}, {name: '50', value: '50'}, {name: '60', value: '60'}],
             aspectRatioList: [{name: '不变', value: 'auto'}, {name: '1:1', value: '1:1'}, {name: '4:3', value: '4:3'}, {name: '3:2', value: '3:2'}, {name: '16:9', value: '16:9'}],
-            audioCodecList: [{name: '不变', value: 'auto'}, {name: 'AAC', value: 'AAC'}, {name: 'flac', value: 'flac'}, {name: 'mp3', value: 'mp3'}, {name: 'mp2', value: 'mp2'}],
+            audioCodecList: [{name: '不变', value: 'auto', container: ['flac', 'flv', 'gif', 'mp3', 'mp4', 'mpg', 'ts']}, {name: 'AAC', value: 'AAC', container: ['flac', 'mp4', 'ts']}, {name: 'mp3', value: 'mp3', container: ['flv', 'mp4', 'ts']}, {name: 'mp2', value: 'mp2', container: ['mpg']}],
             audioProfileList: [{name: '自适应', value: 'auto'}, {name: 'AAC-LC', value: 'AAC-LC'}, {name: 'HE-AAC', value: 'HE-AAC'}, {name: 'HE-AACv2', value: 'HE-AACv2'}],
             audioSampleRateList: [{name: '不变', value: 'auto'}, {name: '22050', value: '22050'}, {name: '32000', value: '32000'}, {name: '44100', value: '44100'}, {name: '48000', value: '48000'}, {name: '96000', value: '96000'}],
             audioChannelsList: [{name: '不变', value: 'auto'}, {name: '1', value: '1'}, {name: '2', value: '2'}, {name: '3', value: '3'}, {name: '4', value: '4'}, {name: '5', value: '5'}, {name: '6', value: '6'}, {name: '7', value: '7'}, {name: '8', value: '8'}, {name: '9', value: '9'}, {name: '10', value: '10'}, {name: '11', value: '11'}, {name: '12', value: '12'}, {name: '13', value: '13'}, {name: '14', value: '14'}, {name: '15', value: '15'}, {name: '16', value: '16'}]
