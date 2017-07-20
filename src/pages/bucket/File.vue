bb45fedb0b0749875fd262557bdea4665ecfd25c
xiaoyu <852483353@qq.com>
2017-07-20 14:11:15 +0800

    modify the css of the File.vue

--- src/pages/bucket/File.vue
+++ src/pages/bucket/File.vue
@@ -594,12 +594,12 @@ const getURL = async (bucket, file, prefix) => {
 <style lang="less" scoped>
 @import '../../styles/index.less';
 
-@layout-bsc-toolbar-border-bottom-color:#f2f1f6;
-@bsc-input-border-color:#d7dde4;
-@input-append-before-background-color:#f5f7f9;
-@input-color:#657180;
+@layout-bsc-toolbar-border-bottom-color: #f2f1f6;
+@bsc-input-border-color: #d7dde4;
+@input-append-before-background-color: #f5f7f9;
+@input-color: #657180;
 
-.@{css-prefix}file{
+.@{css-prefix}file {
     .layout-bsc-toolbar {
         padding-bottom: 8px;
         border-bottom: 1px solid @layout-bsc-toolbar-border-bottom-color;
