72bbccad9ac6eb8668467299aafaee72a3f454cb
lan <adwerrd@hotmail.com>
2017-08-17 11:07:48 +0800

    Build release

---
+++ static/conf.js
@@ -0,0 +1,9 @@
+// shit
+window.domain = 'baishancloud.com'
+window.dashboard_conf = {
+    apiHost: 'api.bscstorage.com:9090',
+    awsHost: 's2.i.qingcdn.com',
+    onlineMode: true,
+    upgradeMode: false,
+    appID: 6
+}
--- static/host.js
+++
@@ -1,6 +0,0 @@
-// shit
-window.domain = 'bscstorage.com'
-window.dashboard_conf = {
-    apiHost: 'frontendapi-stats.bscstorage.com:9090',
-    awsHost: 's2.i.qingcdn.com'
-}
