d7089fe5e410d50dfc2a778955b5fd819e84daa5
lan <adwerrd@hotmail.com>
2017-08-17 14:04:43 +0800

    Build release

--- static/conf.js
+++ static/conf.js
@@ -3,7 +3,8 @@ window.domain = 'baishancloud.com'
 window.dashboard_conf = {
     apiHost: 'api.bscstorage.com:9090',
     awsHost: 's2.i.qingcdn.com',
-    onlineMode: true,
-    upgradeMode: false,
+    transcoderHOST: 'transcoder-ss.bscstorage.com',
+    onlineMode: 'True',
+    upgradeMode: 'False',
     appID: 6
 }
