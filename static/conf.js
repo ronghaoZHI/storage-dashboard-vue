646902ba69ae3e7540e723459d4f284d15546202
lan <adwerrd@hotmail.com>
2017-08-18 14:07:58 +0800

    Build release

--- static/conf.js
+++ static/conf.js
@@ -1,7 +1,7 @@
 // shit
 window.domain = 'baishancloud.com'
 window.dashboard_conf = {
-    apiHost: 'api.bscstorage.com:9090',
+    apiHost: 'dashboardapi-ss.cwn.baishancloud.com:9090',
     awsHost: 's2.i.qingcdn.com',
     transcoderHOST: 'transcoder-ss.bscstorage.com',
     onlineMode: 'True',
