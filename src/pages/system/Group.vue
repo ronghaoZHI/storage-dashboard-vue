3010509ef24d93b79a6dff1f9e98f47784e40b6b
lan <adwerrd@hotmail.com>
2017-10-12 16:11:41 +0800

    add list sort

--- src/pages/system/Group.vue
+++ src/pages/system/Group.vue
@@ -20,31 +20,23 @@
         </div>
         <div class="content">
             <div class="section-chart-tab">
-                <button v-bind:class="{buttonFocus: showChart === 0}" @click="tabToggle(0,'capacityLine')">创建时间升序</button>
-                <button v-bind:class="{buttonFocus: showChart === 1}" @click="tabToggle(1,'uploadTrafficLine')">CPU</button>
-                <button v-bind:class="{buttonFocus: showChart === 2}" @click="tabToggle(2,'downloadTrafficLine')">IO</button>
-                <button v-bind:class="{buttonFocus: showChart === 3}" @click="tabToggle(3,'downloadsLine')">容量</button>
+                <button v-bind:class="{buttonFocus: showChart === 'group_id'}" @click="tabToggle('group_id')">创建时间升序</button>
+                <button v-bind:class="{buttonFocus: showChart === 'cpu'}" @click="tabToggle('cpu')">CPU</button>
+                <button v-bind:class="{buttonFocus: showChart === 'ioutil'}" @click="tabToggle('ioutil')">IO</button>
+                <button v-bind:class="{buttonFocus: showChart === 'space'}" @click="tabToggle('space')">容量</button>
                 <div class="refresh-section">
-                    <span @click="getGroupList"><Icon type="refresh" size="20"></Icon></span>
+                    <span @click="getGroupList(false, true)"><Icon type="refresh" size="20"></Icon></span>
                 </div>
             </div>
             <div class="section-chart">
                 <Spin size="bigger" fix v-if="spinShow"></Spin>
-                <div class="card-chart" v-show="showChart === 0">
-                    <group-card v-for="group in groupList" :key="group.group_id" :data="group"></group-card>
+                <div class="card-chart">
+                    <group-card v-for="group in groupList" :key="group.group_id" :data="group" :sortBy="showChart"></group-card>
                     <div class="show-more" v-show="nextGroupId !== null" @click="getGroupList(true)">
                         <p>show more</p>
                         <Icon type="chevron-down"></Icon>
                     </div>
                 </div>
-                <div class="card-chart" v-show="showChart === 1">
-                    
-                </div>
-                <div class="card-chart" v-show="showChart === 2">
-                    
-                </div>
-                <div class="card-chart" v-show="showChart === 3"></div>
-                    
             </div>
         </div>
     </div>
@@ -58,7 +50,7 @@ export default {
             searchType: 'group_id',
             searchValue: '',
             read_only: 0,
-            showChart: 0,
+            showChart: 'group_id',
             groupList: [],
             nextGroupId: 0,
             spinShow: true,
@@ -75,7 +67,7 @@ export default {
         tabToggle (index, ref) {
             this.showChart = index
         },
-        async getGroupList (isAppend = false) {
+        async getGroupList (isAppend = false, cleanParams = false) {
             this.spinShow = true
             this.$Loading.start()
             try {
@@ -86,20 +78,48 @@ export default {
                     read_only: this.read_only
                 }})
                 this.nextGroupId = groupData.next_group_id
-                this.groupList = isAppend ? this.groupList.concat(groupData.group) : groupData.group
+                this.addPartitionCountToGroup(groupData.group)
+                this.groupList = isAppend ? this.getSortedGroupList(this.groupList.concat(groupData.group), this.showChart) : this.getSortedGroupList(groupData.group, this.showChart)
                 this.spinShow = false
                 this.$Loading.finish()
             } catch (error) {
                 this.spinShow = false
                 this.$Loading.error()
             }
+        },
+        addPartitionCountToGroup (list) {
+            list.forEach(group => {
+                group.cpu = 0
+                group.ioutil = 0
+                group.space = 0
+                group.partition.length > 0 && group.partition.forEach(partition => {
+                    if (partition.cpu > group.cpu) group.cpu = partition.cpu
+                    if (partition.ioutil > group.ioutil) group.ioutil = partition.ioutil
+                    if (partition.space > group.space) group.space = partition.space
+                })
+            })
+        },
+        getSortedGroupList (list, sortBy) {
+            return list.length > 0 && list.sort((a, b) => {
+                if (sortBy === 'group_id') {
+                    return a.group_id - b.group_id
+                } else {
+                    return b[sortBy] - a[sortBy]
+                }
+            })
+        },
+        cleanParams () {
+            this.nextGroupId = 0
+            this.start_group_id = 0
+            this.searchValue = ''
         }
     },
     watch: {
         'read_only' (to, from) {
-            this.start_group_id = 0
-            this.searchValue = ''
-            this.getGroupList()
+            this.getGroupList(false, true)
+        },
+        'showChart' (to, from) {
+            this.getSortedGroupList(this.groupList, to)
         }
     }
 }
