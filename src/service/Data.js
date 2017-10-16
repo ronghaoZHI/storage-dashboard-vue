0a1b9021a9ee02710603eba094615d8db49a7f69
lan <adwerrd@hotmail.com>
2017-10-16 19:28:14 +0800

    bugfix switch user

--- src/service/Data.js
+++ src/service/Data.js
@@ -1,13 +1,7 @@
 import { handler } from '@/service/Aws'
-import bucket from '@/store/modules/bucket'
 
 const getBucketList = async () => {
-    let bucketListStore = bucket.state.bucketList
-    if (bucketListStore > 0) {
-        return bucketListStore
-    } else {
-        return bucket.state.bucketList = await handler('listBuckets')
-    }
+    return await handler('listBuckets')
 }
 
 export { getBucketList }
