f2104e0ba39a6ebab7e4ff4fb9dcd48692a9b0ca
lan <adwerrd@hotmail.com>
2017-12-05 11:25:23 +0800

    for https

--- src/service/API.js
+++ src/service/API.js
@@ -4,15 +4,15 @@ const VERSION = '/v1/'
 const VERSION_TRANSCODER = '/2012-09-25/'
 
 const group = (url) => '//' + HOST.apiHost + VERSION + url
-const groupTranscoder = (url) => 'http://' + HOST.transcoderHOST + VERSION_TRANSCODER + url
+const groupTranscoder = (url) => '//' + HOST.transcoderHOST + VERSION_TRANSCODER + url
 
 export const LOGIN = group('auth/login')
 export const LOGOUT = group('auth/logout')
 export const REPASSWORD = group('user/update/password')
 export const USERINFO = group('profile')
 export const ACCESSKEY = group('accesskey')
-export const SSO_CHECK_LOGIN = (callback = '') => `http://uc.baishancloud.com/web/user/checkLogin?language=1&appId=${window.dashboard_conf.appID}`
-export const SSO_LOGOUT = `http://uc.baishancloud.com/web/user/logout?language=1&appId=${window.dashboard_conf.appID}`
+export const SSO_CHECK_LOGIN = (callback = '') => `https://uc.baishancloud.com/web/user/checkLogin?language=1&appId=${window.dashboard_conf.appID}`
+export const SSO_LOGOUT = `https://uc.baishancloud.com/web/user/logout?language=1&appId=${window.dashboard_conf.appID}`
 
 export const CREATE_USER = group('administrator/create/user')
 export const REDIRECT_BUCKET = group('superuser/redirect/bucket')
