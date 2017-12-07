55ed056223e04c69f06531df4c2ff1776c1f975d
zyl <949472497@qq.com>
2017-12-07 15:38:02 +0800

    Build release

--- static/conf.js
+++ static/conf.js
@@ -4,7 +4,7 @@ window.dashboard_conf = {
     apiHost: 'dashboardapi-ss.cwn.baishancloud.com:9090',
     awsHost: 's2.i.qingcdn.com',
     transcoderHOST: 'transcoder-ss.bscstorage.com',
-    onlineMode: 'True',
+    onlineMode: 'False',
     upgradeMode: 'False',
     appID: '6'
 }
