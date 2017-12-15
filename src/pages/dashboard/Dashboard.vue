dd68a1d9270a166a4e45689fff28c6d565aab6bc
xiaoyu <852483353@qq.com>
2017-12-15 12:37:24 +0800

    modify dashboard dateDivided

--- src/pages/dashboard/Dashboard.vue
+++ src/pages/dashboard/Dashboard.vue
@@ -124,8 +124,8 @@ export default {
     data () {
         return {
             showChart: 0,
-            dateDividedBefore: '20171231',
-            dateDivided: '20180101',
+            dateDividedBefore: '20171130',
+            dateDivided: '20171201',
             bucketList: this.bucketList,
             selectedBucket: 'All Buckets',
             dateDefault: {
