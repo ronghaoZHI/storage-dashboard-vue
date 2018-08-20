3d14c408c9892facd38d0bd5c39050bd58074466
lan <adwerrd@hotmail.com>
2018-08-20 17:04:34 +0800

    fix vue-echarts webpack babel error

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -47,7 +47,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test'), resolve('node_modules/vue-echarts')]
+        include: [resolve('src'), resolve('test'), resolve('node_modules/vue-echarts'), resolve('node_modules/resize-detector')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
