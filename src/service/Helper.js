c5c835ea72c191333e75eba21b0888cfc6f17b47
lan <adwerrd@hotmail.com>
2018-08-27 20:03:39 +0800

    format

--- src/service/Helper.js
+++ src/service/Helper.js
@@ -7,11 +7,11 @@ import { SSO_LOGOUT } from '@/service/API'
 export const logout = async (message) => {
   message
     ? iView.Modal.error({
-        title: 'Need login',
-        content: `${message},need to login again`,
-        onOk: () => dataClearAndLocation(),
-        okText: 'OK'
-      })
+      title: 'Need login',
+      content: `${message},need to login again`,
+      onOk: () => dataClearAndLocation(),
+      okText: 'OK'
+    })
     : dataClearAndLocation()
 }
 
@@ -56,49 +56,49 @@ export function getCookie(name) {
 }
 
 export function sentMessage(message, stak = '') {
-  if(process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === 'production') {
     const msg = {
-    attachments: [
-      {
-        "fallback": "Required plain-text summary of the attachment.",
-        "color": "#36a64f",
-        "pretext": "出错了",
-        "author_name": "CWN-Alert",
-        "author_link": "http://flickr.com/bobby/",
-        "author_icon": "http://flickr.com/icons/bobby.jpg",
-        "title": "错误",
-        "text": message,
-        "fields": [
-          {
-            "title": "Url",
-            "value": window.location.href,
-            "short": false
-          },
-          {
-            "title": "store",
-            "value": JSON.stringify({
-              token: store.default.state.token,
-              user: store.default.state.user
-            }),
-            "short": false
-          },
-          {
-            "title": "stack",
-            "value": JSON.stringify(stak),
-            "short": false
-          }
-        ],
-        "image_url": "http://my-website.com/path/to/image.jpg",
-        "thumb_url": "http://example.com/path/to/thumb.png",
-        "footer": "CWN-dashboard",
-        "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-        "ts": 123456789
-      }
-    ]
-  }
-  fetch('https://hooks.slack.com/services/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', {
-    method: 'post',
-    body: JSON.stringify(msg)
-  })
+      attachments: [
+        {
+          "fallback": "Required plain-text summary of the attachment.",
+          "color": "#36a64f",
+          "pretext": "出错了",
+          "author_name": "CWN-Alert",
+          "author_link": "http://flickr.com/bobby/",
+          "author_icon": "http://flickr.com/icons/bobby.jpg",
+          "title": "错误",
+          "text": message,
+          "fields": [
+            {
+              "title": "Url",
+              "value": window.location.href,
+              "short": false
+            },
+            {
+              "title": "store",
+              "value": JSON.stringify({
+                token: store.default.state.token,
+                user: store.default.state.user
+              }),
+              "short": false
+            },
+            {
+              "title": "stack",
+              "value": JSON.stringify(stak),
+              "short": false
+            }
+          ],
+          "image_url": "http://my-website.com/path/to/image.jpg",
+          "thumb_url": "http://example.com/path/to/thumb.png",
+          "footer": "CWN-dashboard",
+          "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+          "ts": 123456789
+        }
+      ]
+    }
+    fetch('https://hooks.slack.com/services/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', {
+      method: 'post',
+      body: JSON.stringify(msg)
+    })
   }
 }
