8b019cbb3e7b0663baaecc2b10823af1fa15bf40
lan <adwerrd@hotmail.com>
2018-09-14 17:01:14 +0800

    add prerender for login page

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
@@ -17,6 +19,16 @@ module.exports = {
       (config) => config.output.chunkFilename('[contenthash].bundle.js'),
       (config) => config.performance.hints(false),
     )
+  },
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        // Required - The path to the webpack-outputted app to prerender.
+        staticDir: path.join(__dirname, 'dist'),
+        // Required - Routes to render.
+        routes: ['/', '/login', '/bridge'],
+      }),
+    ],
   },
   devServer: {
     disableHostCheck: true,
